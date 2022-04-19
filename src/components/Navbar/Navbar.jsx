4dff81fe37fa08683940f9b8d402f0b32232311b
David Gonzalez <davidadriel64@gmail.com>
2022-04-19 20:21:05 -0300

    Desafío: Menú e-commerce

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <h1 className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">TuTienda</h1>
+      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">Muebles</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">Electronica</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">Ropa</a></li>
+      </ul>
+
+      <div className="col-md-3 text-end">
+        <button type="button" className="btn btn-outline-primary me-2">Ingresar</button>
+        <button type="button" className="btn btn-primary mx-2">Registrarse</button>
+      </div>
+    </header>
+  </div>
+  )
+}
+
+export default Navbar
